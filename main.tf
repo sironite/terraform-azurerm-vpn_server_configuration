65784c386bb3d554617c3bdda8166c40411fe5e3
TheIronRock95 <58424695+TheIronRock95@users.noreply.github.com>
2023-07-09 21:11:14 +0200

    fix: module

--- main.tf
+++ main.tf
@@ -6,52 +6,19 @@ resource "azurerm_vpn_server_configuration" "example" {
   vpn_protocols            = var.vpn_protocols
 
   dynamic "azure_active_directory_authentication" {
-    for_each = var.vpn_authentication_types == "AzureAD" ? [1] : []
+    for_each = contains(var.vpn_authentication_types, "AAD") ? [1] : []
     content {
       audience  = var.audience
       issuer    = var.issuer
-      tenant_id = var.tenant_id
+      tenant    = var.tenant_id
     }
   }
 
   dynamic "client_root_certificate" {
-    for_each = var.vpn_authentication_types == "Certificate" ? [1] : []
+    for_each = contains(var.vpn_authentication_types, "Certificate") ? [1] : []
     content {
       name             = var.client_root_certificate_name
       public_cert_data = var.public_cert_data
     }
-  }
-
-  dynamic "radius" {
-    for_each = var.vpn_authentication_types == "Radius" ? [1] : []
-    content {
-      radius {
-        server {
-          adress = var.server_adress
-          secret = var.server_secret
-          score  = var.server_score
-        }
-        client_root_certificate {
-          name      = var.client_root_certificate_name
-          tumbprint = var.tumbprint
-        }
-        server_root_certificate {
-          name             = var.server_root_certificate_name
-          public_cert_data = var.public_cert_data
-        }
-      }
-    }
-  }
-
-
-  ipsec_policy {
-    dh_group               = var.dh_group
-    ike_encryption         = var.ike_encryption
-    ike_integrity          = var.ike_integrity
-    ipsec_encryption       = var.ipsec_encryption
-    ipsec_integrity        = var.ipsec_integrity
-    pfs_group              = var.pfs_group
-    sa_data_size_kilobytes = var.sa_data_size_kilobytes
-    sa_life_time_seconds   = var.sa_life_time_seconds
   }
 }
--- variables.tf
+++ variables.tf
@@ -15,15 +15,15 @@ variable "location" {
 }
 
 variable "vpn_authentication_types" {
-  type        = string
+  type        = list(string)
   description = "The type of VPN authentication to use."
-  default     = null
+  default     = []
 }
 
 variable "vpn_protocols" {
-  type        = string
+  type        = list(string)
   description = "The VPN protocols to use."
-  default     = null
+  default     = []
 }
 
 variable "audience" {
@@ -41,95 +41,5 @@ variable "issuer" {
 variable "tenant_id" {
   type        = string
   description = "The tenant ID for Azure AD authentication."
-  default     = null
-}
-
-variable "client_root_certificate_name" {
-  type        = string
-  description = "The name of the client root certificate."
-  default     = null
-}
-
-variable "public_cert_data" {
-  type        = string
-  description = "The public certificate data."
-  default     = null
-}
-
-variable "server_adress" {
-  type        = string
-  description = "The address of the RADIUS server."
-  default     = null
-}
-
-variable "server_secret" {
-  type        = string
-  description = "The secret for the RADIUS server."
-  default     = null
-}
-
-variable "server_score" {
-  type        = string
-  description = "The score for the RADIUS server."
-  default     = null
-}
-
-variable "tumbprint" {
-  type        = string
-  description = "The thumbprint for the client root certificate."
-  default     = null
-}
-
-variable "server_root_certificate_name" {
-  type        = string
-  description = "The name of the server root certificate."
-  default     = null
-}
-
-variable "dh_group" {
-  type        = string
-  description = "The Diffie-Hellman group to use."
-  default     = null
-}
-
-variable "ike_encryption" {
-  type        = string
-  description = "The IKE encryption to use."
-  default     = null
-}
-
-variable "ike_integrity" {
-  type        = string
-  description = "The IKE integrity to use."
-  default     = null
-}
-
-variable "ipsec_encryption" {
-  type        = string
-  description = "The IPSec encryption to use."
-  default     = null
-}
-
-variable "ipsec_integrity" {
-  type        = string
-  description = "The IPSec integrity to use."
-  default     = null
-}
-
-variable "pfs_group" {
-  type        = string
-  description = "The Perfect Forward Secrecy group to use."
-  default     = null
-}
-
-variable "sa_data_size_kilobytes" {
-  type        = string
-  description = "The size of the SA data in kilobytes."
-  default     = null
-}
-
-variable "sa_life_time_seconds" {
-  type        = string
-  description = "The lifetime of the SA in seconds."
   default     = null
 }
